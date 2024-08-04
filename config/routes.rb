5ecfa948ca33c9d48a7532e932867f2cd21f9904
emilia grama <emiliagrama@gmail.com>
2024-08-04 14:29:28 +0200

    social medias x2 and some front

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  
-  # Health check route
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'biography/show'
+  get 'home/index'
 
-  # Define the root path route
   root "home#index"
-
-  # Route for the Biography controller
-  get 'biography', to: 'biography#show'
+  get "biography", to: "biography#show"
 end
